a4c8d36653828cad41de7686d228638f448e24e3
MannyHuang <>
2019-01-02 20:56:36 +0800

    added method 5-8

--- scripts/feature_selection.py
+++ scripts/feature_selection.py
@@ -130,6 +130,47 @@ def get_feature_indices(data_set, feature_set):
                 fea_index.append(A_col)
     return fea_index
 
+# class LR
+class LR(LogisticRegression):
+    def __init__(self, threshold=0.01, dual=False, tol=1e-4, C=1.0,
+                 fit_intercept=True, intercept_scaling=1, class_weight=None,
+                 random_state=None, solver='liblinear', max_iter=100,
+                 multi_class='ovr', verbose=0, warm_start=False, n_jobs=1):
+
+        self.threshold = threshold
+        LogisticRegression.__init__(self, penalty='l1', dual=dual, tol=tol, C=C,
+                 fit_intercept=fit_intercept, intercept_scaling=intercept_scaling, class_weight=class_weight,
+                 random_state=random_state, solver=solver, max_iter=max_iter,
+                 multi_class=multi_class, verbose=verbose, warm_start=warm_start, n_jobs=n_jobs)
+ 
+        self.l2 = LogisticRegression(penalty='l2', dual=dual, tol=tol, C=C, fit_intercept=fit_intercept, intercept_scaling=intercept_scaling, class_weight = class_weight, random_state=random_state, solver=solver, max_iter=max_iter, multi_class=multi_class, verbose=verbose, warm_start=warm_start, n_jobs=n_jobs)
+
+    def fit(self, X, y, sample_weight=None):
+     
+        super(LR, self).fit(X, y, sample_weight=sample_weight)
+        self.coef_old_ = self.coef_.copy()
+        self.l2.fit(X, y, sample_weight=sample_weight)
+
+        cntOfRow, cntOfCol = self.coef_.shape
+
+        for i in range(cntOfRow):
+            for j in range(cntOfCol):
+                coef = self.coef_[i][j]
+      
+                if coef != 0:
+                    idx = [j]
+       
+                    coef1 = self.l2.coef_[i][j]
+                    for k in range(cntOfCol):
+                        coef2 = self.l2.coef_[i][k]
+     
+                        if abs(coef1-coef2) < self.threshold and j != k and self.coef_[i][k] == 0:
+                            idx.append(k)
+
+                    mean = coef / len(idx)
+                    self.coef_[i][idx] = mean
+        return self
+
 
 '''
 Loading Data
@@ -244,12 +285,13 @@ clf_5.feature_importances_
 model_5 = SelectFromModel(clf_5, prefit=True)
 features_5 = model_5.transform(X)
 
-# method6: LinearSVC
-lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(X, y)
-model = SelectFromModel(lsvc, prefit=True)
-features_6 = model.transform(X)
+# method6: Logistic Regression
+features_6 = RFE(estimator=LogisticRegression(), n_features_to_select=35).fit_transform(X, y)
 
+# method7: LR based on L1 and L2
+features_7 = SelectFromModel(LR(threshold=0.5, C=0.1)).fit_transform(X, y)
 
+# method: LR based on L1 and L2
 
 # output features as csv
 np.savetxt(os.path.join(out_dir, 'features_2.csv'), features_2, delimiter=",")
@@ -267,8 +309,6 @@ feature_indices = get_feature_indices(data_set = features, feature_set = feature
 
 
 """
-# 2. Wrapper
-RFE(estimator=LogisticRegression(), n_features_to_select=2).fit_transform(X, y)
 
 
 # 3. Embedded
